7eb562851f2961cdd51f643d183cd09a5b1f88c0
Replit user <>
2023-03-23 08:18:56 +0000

    add array size check

---
+++ .ccls-cache/@home@runner@lab3-task1-gr14-Zavadsky/include@interface.h
@@ -0,0 +1,14 @@
+//created by Kirill Zavadsky 20.03.2023
+
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int rearrangeDigits(int num);
+
+void generateRandomArray(int *arr, int size);
+
+#endif 
--- src/main.c
+++ src/main.c
@@ -1,8 +1,29 @@
-/* hello.c */
-#include <stdio.h>
+//created by Kirill Zavadsky 20.03.2023
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+#include "../include/interface.h"
+// #include <stdio.h>
+// #include <stdlib.h>
+
+int main () {
+  int size;
+  printf("Enter the size of the array: ");
+  scanf("%d", &size);
+
+
+  int *arr = (int *) malloc(size * sizeof(int));
+
+  generateRandomArray(arr, size);
+
+  for(int i = 0; i < size; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("%c", '\n');
+  for (int i = 0; i < size; i++) {
+    int result = rearrangeDigits(arr[i]);
+    printf("%d ", result);
+  }
+  printf("\n");
+
+  free(arr);
+  return 0;
 }
